b093e43542672a0e0a745ae651e9746d67273b1c
evanbones <74276616+evanbones@users.noreply.github.com>
2026-09-10 00:02:02 -0700

    remove intoxication from vinegar

--- src/main/java/com/baisylia/culturaldelights/item/ModFoods.java
+++ src/main/java/com/baisylia/culturaldelights/item/ModFoods.java
@@ -96,8 +96,7 @@ public class ModFoods {
 
     public static final FoodProperties ACID = (new FoodProperties.Builder()).nutrition(0).saturationMod(0.8F)
             .effect(() -> new MobEffectInstance(MobEffects.HARM, 1200, 0), 1.0F).build();
-    public static final FoodProperties VINEGAR = (new FoodProperties.Builder()).nutrition(0).saturationMod(0.8F)
-            .effect(() -> new MobEffectInstance(com.baisylia.culturaldelights.effect.ModEffects.INTOXICATION.get(), 1200, 0), 1.0F).build();
+    public static final FoodProperties VINEGAR = (new FoodProperties.Builder()).nutrition(0).saturationMod(0.8F).build();
     public static final FoodProperties PICKLED_EGG = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.5F).build();
     public static final FoodProperties CHEESE_WHEEL = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.12F)
             .effect(() -> new MobEffectInstance(com.baisylia.culturaldelights.effect.ModEffects.CHEESY.get(), 200, 1), 1.0F).fast().build();
